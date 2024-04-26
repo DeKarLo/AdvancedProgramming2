8ce277c8406cb04ebd4b17166cab1fc4fcd0a674
DeKarLo <karen.ananyan2017@mail.ru>
2024-04-27 00:02:53 +0500

    add routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,14 +1,18 @@
-package api
+package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
+	"karen.assignment/shoup/internal/models"
 )
 
 // RegisterRoutes registers all API routes
-func RegisterRoutes(router *mux.Router) {
-	// Define routes
-	router.HandleFunc("/signup", signUpHandler).Methods("POST")
-	router.HandleFunc("/login", loginHandler).Methods("POST")
-	router.HandleFunc("/items", getItemsHandler).Methods("GET")
-	router.HandleFunc("/order", placeOrderHandler).Methods("POST")
+func RegisterRoutes(router *mux.Router, userRepo *models.UserRepository, itemRepo *models.ItemRepository, orderRepo *models.OrderRepository) {
+	router.HandleFunc("/v1/signup", signUpHandler(userRepo)).Methods("POST")
+	router.HandleFunc("/v1/login", loginHandler(userRepo)).Methods("POST")
+	router.HandleFunc("/v1/items", getItemsHandler(itemRepo)).Methods("GET")
+	router.HandleFunc("/v1/order", func(w http.ResponseWriter, r *http.Request) {
+		AuthMiddleware(placeOrderHandler(orderRepo, itemRepo)).ServeHTTP(w, r)
+	}).Methods("POST")
 }
